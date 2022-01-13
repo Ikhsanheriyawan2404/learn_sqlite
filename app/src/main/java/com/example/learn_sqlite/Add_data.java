ddad1663c7ab4354a0871084d9a638bebce2bbca
ikhsanheriyawan2404 <ikhsanheriyawan2404@gmail.com>
2022-01-13 13:21:47 +0700

    Tetek bengek

--- app/src/main/java/com/example/learn_sqlite/Add_data.java
+++ app/src/main/java/com/example/learn_sqlite/Add_data.java
@@ -3,12 +3,40 @@ package com.example.learn_sqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Add_data extends AppCompatActivity {
+
+    BiodataTable biodataTable;
+    EditText name, address;
+    Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_data);
+
+        name = findViewById(R.id.name);
+        address = findViewById(R.id.address);
+        save = findViewById(R.id.save);
+
+        biodataTable = new BiodataTable(getApplicationContext());
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save_data();
+            }
+        });
+    }
+
+    void save_data()
+    {
+        biodataTable.save_data(
+                name.getText().toString(),
+                address.getText().toString()
+        );
     }
 }
--- app/src/main/java/com/example/learn_sqlite/MainActivity.java
+++ app/src/main/java/com/example/learn_sqlite/MainActivity.java
@@ -2,13 +2,134 @@ package com.example.learn_sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listview;
+    Button add_data;
+    ArrayList<Object> list;
+
+    SQLiteDatabase database;
+    Cursor cursor;
+    BiodataTable biodataTable;
+    Custome_adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listview = findViewById(R.id.listview);
+        add_data = findViewById(R.id.add_data);
+
+        add_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Add_data.class));
+            }
+        });
+        get_data();
+    }
+
+    void get_data()
+    {
+        list = new ArrayList<Object>();
+        cursor = biodataTable.show_data();
+        if (cursor != null && cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                String id = cursor.getString(0);
+                String name = cursor.getString(1);
+                String address = cursor.getString(2);
+                list.add(new Object(
+                        id,
+                        name,
+                        address
+                ));
+            }
+            adapter = new Custome_adapter(getApplicationContext(), list);
+            listview.setAdapter(adapter);
+        }
+    }
+}
+
+class Custome_adapter extends BaseAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+    ArrayList<Object> model;
+
+    public Custome_adapter(Context context, ArrayList<Object> list) {
+        this.context = context;
+        this.model = model;
+        layoutInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return model.size();
+    }
+
+    @Override
+    public java.lang.Object getItem(int position) {
+        return model.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    TextView id, name, address;
+    Button edit, delete;
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        View view1 = layoutInflater.inflate(R.layout.list_data, viewGroup, false);
+
+        id = view1.findViewById(R.id.id);
+        name = view1.findViewById(R.id.name);
+        address = view1.findViewById(R.id.address);
+
+        id.setText(model.get(position).getId());
+        name.setText(model.get(position).getName());
+        address.setText(model.get(position).getAddress());
+
+        edit = view1.findViewById(R.id.edit);
+        delete = view1.findViewById(R.id.delete);
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, Edit_data.class);
+                intent.putExtra("name", model.get(position).getName());
+                intent.putExtra("address", model.get(position).getAddress());
+                intent.putExtra("id", model.get(position).getId());
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context.getApplicationContext(), "Data deleted!",Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view1;
     }
 }
